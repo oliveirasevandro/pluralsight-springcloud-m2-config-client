99fa4eb69060c33e18ea3de372199cad8f97cff4
Evandro Oliveira <evandro.oliveira@matera.com>
2017-03-28 10:24:16 -0300

    Initial commit

---
+++ src/main/java/pluralsight/demo/Application.java
@@ -0,0 +1,15 @@
+package pluralsight.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by eso on 28/03/17.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
